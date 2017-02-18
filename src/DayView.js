fb1d5e0786eda728b107b763f5349eb17da0d3ba
Guillaume Salles <guillaume.salles@me.com>
2017-02-18 15:04:32 -0500

    Fix horizontal scroll issue

--- src/DayView.js
+++ src/DayView.js
@@ -26,7 +26,7 @@ class DayView extends React.Component {
 
 	render() {
 		return (
-			<div style={{ height: '100%', position: 'relative', overflowY: 'hidden' }}>
+			<div style={{ height: '100%', width: '100%', position: 'relative', overflowY: 'hidden' }}>
 				<div 
 					ref={elem => {
 						if(elem != null) {
